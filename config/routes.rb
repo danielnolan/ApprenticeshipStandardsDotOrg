c9a5da1416e9ef304d685a828043e3a84b66c9a1
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-23 13:01:50 -0800

    Add subdomain requirement for Devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users,
-    skip: :registrations,
-    controllers: {
-      sessions: "users/sessions"
-    }
-    as :user do
-      get "users/edit", to: "devise/registrations#edit", as: "edit_user_registration"
-      patch "users", to: "devise/registrations#update", as: "user_registration"
-      put "users", to: "devise/registrations#update", as: nil
-    end
+  constraints(Subdomain) do
+    devise_for :users,
+      skip: :registrations,
+      controllers: {
+        sessions: "users/sessions"
+      }
+      as :user do
+        get "users/edit", to: "devise/registrations#edit", as: "edit_user_registration"
+        patch "users", to: "devise/registrations#update", as: "user_registration"
+        put "users", to: "devise/registrations#update", as: nil
+      end
+  end
 
   root to: "standards_imports#new"
   resources :standards_imports, only: [:new, :create, :show]
