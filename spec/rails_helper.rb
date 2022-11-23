add619ac94986f6606c1e8dd690cff5234fcc148
Jeanine Soterwood <jeanine@thoughtbot.com>
2022-11-23 13:02:37 -0800

    Add session request spec to check admin domain behavior

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,4 +76,8 @@ RSpec.configure do |config|
   config.before(:each, type: :system, debug: true) do
     driven_by :selenium_chrome
   end
+
+  config.before(:each, type: :request, admin: true) do
+    host! "admin.example.com"
+  end
 end
