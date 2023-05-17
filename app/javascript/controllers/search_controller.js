08441be3d35ed0b2f51fe42cc4fcd38403e782b6
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-05-17 10:12:19 -0700

    WIP checkbox filter outlets

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,12 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ "textField", "checkbox" ]
+  static outlets = [ "checkbox_filter" ]
 
   clearForm() {
     this.textFieldTargets.forEach(input => input.value = "")
     this.checkboxTargets.forEach(input => input.checked = false)
+    console.log(this.checkboxFilterOutlets)
+    //    this.checkboxOutlets.forEach(type => type.updateCount())
+    //this.dispatch("clearForm")
 
-    const event = new CustomEvent("clearForm");
-    window.dispatchEvent(event);
+//    const event = new CustomEvent("clearForm");
+//    window.dispatchEvent(event);
   }
 }
